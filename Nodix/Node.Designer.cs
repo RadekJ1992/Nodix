1f9d31920699ac2e08cda0d2ed574fa61e2ced04
Radosław Jarzynka <radoslaw.jarzynka@gmail.com>
2013-11-15 22:19:25 +0100

    Węzeł!

---
+++ Nodix/Node.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Nodix {
+    partial class Nodix {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.cloudIPField = new System.Windows.Forms.TextBox();
+            this.cloudIPLabel = new System.Windows.Forms.Label();
+            this.cloudPortLabel = new System.Windows.Forms.Label();
+            this.cloudPortField = new System.Windows.Forms.TextBox();
+            this.managerPortLabel = new System.Windows.Forms.Label();
+            this.managerPortField = new System.Windows.Forms.TextBox();
+            this.managerIPLabel = new System.Windows.Forms.Label();
+            this.managerIPField = new System.Windows.Forms.TextBox();
+            this.connectToCloudButton = new System.Windows.Forms.Button();
+            this.connectToManagerButton = new System.Windows.Forms.Button();
+            this.log = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // cloudIPField
+            // 
+            this.cloudIPField.Location = new System.Drawing.Point(12, 29);
+            this.cloudIPField.Name = "cloudIPField";
+            this.cloudIPField.Size = new System.Drawing.Size(100, 20);
+            this.cloudIPField.TabIndex = 0;
+            this.cloudIPField.Text = "127.0.0.1";
+            // 
+            // cloudIPLabel
+            // 
+            this.cloudIPLabel.AutoSize = true;
+            this.cloudIPLabel.Location = new System.Drawing.Point(12, 13);
+            this.cloudIPLabel.Name = "cloudIPLabel";
+            this.cloudIPLabel.Size = new System.Drawing.Size(54, 13);
+            this.cloudIPLabel.TabIndex = 1;
+            this.cloudIPLabel.Text = "IP chmury";
+            // 
+            // cloudPortLabel
+            // 
+            this.cloudPortLabel.AutoSize = true;
+            this.cloudPortLabel.Location = new System.Drawing.Point(12, 52);
+            this.cloudPortLabel.Name = "cloudPortLabel";
+            this.cloudPortLabel.Size = new System.Drawing.Size(63, 13);
+            this.cloudPortLabel.TabIndex = 3;
+            this.cloudPortLabel.Text = "Port chmury";
+            // 
+            // cloudPortField
+            // 
+            this.cloudPortField.Location = new System.Drawing.Point(12, 68);
+            this.cloudPortField.Name = "cloudPortField";
+            this.cloudPortField.Size = new System.Drawing.Size(100, 20);
+            this.cloudPortField.TabIndex = 2;
+            this.cloudPortField.Text = "13000";
+            // 
+            // managerPortLabel
+            // 
+            this.managerPortLabel.AutoSize = true;
+            this.managerPortLabel.Location = new System.Drawing.Point(118, 52);
+            this.managerPortLabel.Name = "managerPortLabel";
+            this.managerPortLabel.Size = new System.Drawing.Size(71, 13);
+            this.managerPortLabel.TabIndex = 7;
+            this.managerPortLabel.Text = "Port zarządcy";
+            // 
+            // managerPortField
+            // 
+            this.managerPortField.Location = new System.Drawing.Point(118, 68);
+            this.managerPortField.Name = "managerPortField";
+            this.managerPortField.Size = new System.Drawing.Size(100, 20);
+            this.managerPortField.TabIndex = 6;
+            // 
+            // managerIPLabel
+            // 
+            this.managerIPLabel.AutoSize = true;
+            this.managerIPLabel.Location = new System.Drawing.Point(118, 13);
+            this.managerIPLabel.Name = "managerIPLabel";
+            this.managerIPLabel.Size = new System.Drawing.Size(62, 13);
+            this.managerIPLabel.TabIndex = 5;
+            this.managerIPLabel.Text = "IP zarządcy";
+            // 
+            // managerIPField
+            // 
+            this.managerIPField.Location = new System.Drawing.Point(118, 29);
+            this.managerIPField.Name = "managerIPField";
+            this.managerIPField.Size = new System.Drawing.Size(100, 20);
+            this.managerIPField.TabIndex = 4;
+            this.managerIPField.Text = "127.0.0.1";
+            // 
+            // connectToCloudButton
+            // 
+            this.connectToCloudButton.Location = new System.Drawing.Point(15, 95);
+            this.connectToCloudButton.Name = "connectToCloudButton";
+            this.connectToCloudButton.Size = new System.Drawing.Size(97, 43);
+            this.connectToCloudButton.TabIndex = 8;
+            this.connectToCloudButton.Text = "Połącz z chmurą";
+            this.connectToCloudButton.UseVisualStyleBackColor = true;
+            this.connectToCloudButton.Click += new System.EventHandler(this.connectToCloud);
+            // 
+            // connectToManagerButton
+            // 
+            this.connectToManagerButton.Location = new System.Drawing.Point(118, 95);
+            this.connectToManagerButton.Name = "connectToManagerButton";
+            this.connectToManagerButton.Size = new System.Drawing.Size(97, 43);
+            this.connectToManagerButton.TabIndex = 9;
+            this.connectToManagerButton.Text = "Połącz z zarządcą";
+            this.connectToManagerButton.UseVisualStyleBackColor = true;
+            this.connectToManagerButton.Click += new System.EventHandler(this.connectToManager);
+            // 
+            // log
+            // 
+            this.log.BackColor = System.Drawing.SystemColors.Window;
+            this.log.Location = new System.Drawing.Point(12, 145);
+            this.log.Multiline = true;
+            this.log.Name = "log";
+            this.log.ReadOnly = true;
+            this.log.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.log.Size = new System.Drawing.Size(203, 172);
+            this.log.TabIndex = 13;
+            // 
+            // Nodix
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(229, 329);
+            this.Controls.Add(this.log);
+            this.Controls.Add(this.connectToManagerButton);
+            this.Controls.Add(this.connectToCloudButton);
+            this.Controls.Add(this.managerPortLabel);
+            this.Controls.Add(this.managerPortField);
+            this.Controls.Add(this.managerIPLabel);
+            this.Controls.Add(this.managerIPField);
+            this.Controls.Add(this.cloudPortLabel);
+            this.Controls.Add(this.cloudPortField);
+            this.Controls.Add(this.cloudIPLabel);
+            this.Controls.Add(this.cloudIPField);
+            this.Name = "Nodix";
+            this.Text = "Nodix";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox cloudIPField;
+        private System.Windows.Forms.Label cloudIPLabel;
+        private System.Windows.Forms.Label cloudPortLabel;
+        private System.Windows.Forms.TextBox cloudPortField;
+        private System.Windows.Forms.Label managerPortLabel;
+        private System.Windows.Forms.TextBox managerPortField;
+        private System.Windows.Forms.Label managerIPLabel;
+        private System.Windows.Forms.TextBox managerIPField;
+        private System.Windows.Forms.Button connectToCloudButton;
+        private System.Windows.Forms.Button connectToManagerButton;
+        private System.Windows.Forms.TextBox log;
+    }
+}
+
---
+++ Nodix/Node.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Packet;
+
+namespace Nodix {
+    public partial class Nodix : Form {
+        //fuck delegates!
+        delegate void SetTextCallback(string text);
+
+        //otrzymany i wysyłany pakiets
+        private Packet.ATMPacket receivedPacket;
+        private Packet.ATMPacket processedPacket;
+
+        //kolejka pakietów odebranych z chmury - concurrentQueue jest thread-safe, zwykła queue nie
+        private ConcurrentQueue<Packet.ATMPacket> queuedReceivedPackets = new ConcurrentQueue<Packet.ATMPacket>();
+
+        //dane chmury
+        private IPAddress cloudAddress;        //Adres na którym chmura nasłuchuje
+        private Int32 cloudPort;           //port chmury
+
+        //dane zarządcy
+        private IPAddress managerAddress;        //Adres na którym chmura nasłuchuje
+        private Int32 managerPort;           //port chmury
+
+        private IPEndPoint cloudEndPoint;
+        private IPEndPoint managerEndPoint;
+
+        private Socket cloudSocket;
+        private Socket managerSocket;
+
+        private Thread receiveThread;     //wątek służący do odbierania połączeń
+        private Thread sendThread;        // analogicznie - do wysyłania
+
+        private NetworkStream netStream;
+
+        public bool isRunning { get; private set; }     //info czy klient chodzi - dla zarządcy
+
+        public bool isConnectedToCloud { get; private set; } // czy połączony z chmurą?
+        public bool isConnectedToManager { get; private set; } // czy połączony z zarządcą?
+
+        // tablica kierowania
+        // UWAGA!
+
+        // w momencie gdy chcemy ustawić połączenie VP (jak na slajdzie 22 z wykładów TSST to wartość VCI w strukturze 
+        // trzeba ustawić na 65536. Musiałem zaimplementować coś w stylu tej kreski co jest w [a,-] -> [b,-], a skoro
+        // pole VCI ma 16 bitów to ten int zgodnie z założeniami może przybierać wartośći od 0 do 65535, 65536 jest poza
+        //jego zasięgiem
+
+        //
+        private Dictionary<VPIVCI, VPIVCI> VCArray = new Dictionary<VPIVCI,VPIVCI>(new VPIVCIComparer());
+
+        public Nodix() {
+            InitializeComponent();
+            VPIVCI k = new VPIVCI(1, 1);
+            VPIVCI v = new VPIVCI(2, 2);
+            VCArray.Add(k, v);
+        }
+
+        /*
+                    log.AppendText("wysyłam pakiet!\n");
+                    BinaryFormatter bformatter = new BinaryFormatter();
+                    bformatter.Serialize(netStream, packet);
+                    netStream.Close();
+        */
+
+        private void connectToCloud(object sender, EventArgs e) {
+            if (IPAddress.TryParse(cloudIPField.Text, out cloudAddress)) {
+                log.AppendText(DateTime.Now.ToString(@"MM\/dd\/yyyy h\:mm tt") + " Cloud IP set properly as " + cloudAddress.ToString() + " \n");
+            }
+            else {
+                log.AppendText(DateTime.Now.ToString(@"MM\/dd\/yyyy h\:mm tt") + " Error reading cloud IP" + " \n");
+            }
+            if (Int32.TryParse(cloudPortField.Text, out cloudPort)) {
+                log.AppendText(DateTime.Now.ToString(@"MM\/dd\/yyyy h\:mm tt") + " Cloud port set properly as " + cloudPort.ToString() + " \n");
+            }
+            else {
+                log.AppendText(DateTime.Now.ToString(@"MM\/dd\/yyyy h\:mm tt") + " Error reading cloud Port" + " \n");
+            }
+
+            cloudSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPEndPoint endPoint = new IPEndPoint(cloudAddress, cloudPort);
+            try {
+                cloudSocket.Connect(endPoint);
+                isConnectedToCloud = true;
+                receiveThread = new Thread(this.receiver);
+                receiveThread.Start();
+            }
+            catch (SocketException ex) {
+                isConnectedToCloud = false;
+                log.AppendText(DateTime.Now.ToString(@"MM\/dd\/yyyy h\:mm tt") + " Error while connecting to cloud\n");
+                log.AppendText("Wrong IP or port?\n");
+            }
+        }
+
+        private void connectToManager(object sender, EventArgs e) {
+            if (IPAddress.TryParse(managerIPField.Text, out managerAddress)) {
+                log.AppendText(DateTime.Now.ToString(@"MM\/dd\/yyyy h\:mm tt") + " Manager IP set properly as " + managerAddress.ToString() + " \n");
+            }
+            else {
+                log.AppendText(DateTime.Now.ToString(@"MM\/dd\/yyyy h\:mm tt") + " Error reading manager IP" + " \n");
+            }
+            if (Int32.TryParse(managerPortField.Text, out managerPort)) {
+                log.AppendText(DateTime.Now.ToString(@"MM\/dd\/yyyy h\:mm tt") + " Manager port set properly as " + managerPort.ToString() + " \n");
+            }
+            else {
+                log.AppendText(DateTime.Now.ToString(@"MM\/dd\/yyyy h\:mm tt") + " Error reading manager Port" + " \n");
+            }
+        }
+
+        private void receiver() {
+            NetworkStream networkStream = new NetworkStream(cloudSocket);
+            BinaryFormatter bf = new BinaryFormatter();
+            receivedPacket = (Packet.ATMPacket)bf.Deserialize(networkStream);
+            queuedReceivedPackets.Enqueue(receivedPacket);
+            networkStream.Close();
+            sendThread = new Thread(this.sender);
+            sendThread.Start();
+            receiver();
+        }
+
+        private void SetText(string text) {
+            // InvokeRequired required compares the thread ID of the 
+            // calling thread to the thread ID of the creating thread. 
+            // If these threads are different, it returns true. 
+            if (this.log.InvokeRequired) {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else {
+                this.log.AppendText(text);
+            }
+        }
+
+        private void sender() {
+            if (!queuedReceivedPackets.IsEmpty) {
+                queuedReceivedPackets.TryDequeue(out processedPacket);
+                VPIVCI VCConKey = new VPIVCI();
+                VPIVCI VPConKey = new VPIVCI();
+                VPIVCI value = new VPIVCI();
+                VCConKey.VPI = processedPacket.VPI;
+                VCConKey.VCI = processedPacket.VCI;
+                VPConKey.VPI = processedPacket.VPI;
+                VPConKey.VCI = 65536;
+                NetworkStream networkStream = new NetworkStream(cloudSocket);
+                if (VCArray.ContainsKey(VCConKey)) {
+                    if (VCArray.TryGetValue(VCConKey, out value)) {
+                        SetText("Przekierowywanie [" + processedPacket.VPI + ";" + processedPacket.VCI + "]->[" + value.VPI + ";" + value.VCI + "]\n");
+                        processedPacket.VPI = value.VPI;
+                        processedPacket.VCI = value.VCI;
+                        //TODO co z portami?
+                        BinaryFormatter bformatter = new BinaryFormatter();
+                        bformatter.Serialize(networkStream, processedPacket);
+                        networkStream.Close();
+                    }
+                    else {
+                        SetText("Coś poszło nie tak przy przepisywaniu wartości VPI i VCI z VCArray\n");
+                    }
+                }
+                else if (VCArray.ContainsKey(VPConKey)) {
+                    if (VCArray.TryGetValue(VPConKey, out value)) {
+
+                        SetText("Przekierowywanie [" + processedPacket.VPI + ";" + processedPacket.VCI + "]->[" + value.VPI + ";" + value.VCI + "]\n");
+                        processedPacket.VPI = value.VPI;
+                        // VCI bez zmian
+                        //TODO co z portami?
+                        BinaryFormatter bformatter = new BinaryFormatter();
+                        bformatter.Serialize(netStream, processedPacket);
+                        networkStream.Close();
+                    }
+                    else {
+                        SetText("Coś poszło nie tak przy przepisywaniu wartości VPI i VCI z VCArray\n");
+                    }
+                }
+                else {
+                    SetText("Pakiet stracony - brak odpowiedniego wpisu w tablicy\n");
+                }
+            }
+        }
+    }
+}
