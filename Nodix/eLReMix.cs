e1c216b19c9bdf51c115d0b88874f3168584e7dc
vainamonen <vainamonen@gmail.com>
2014-01-18 15:07:32 +0100

    początek eLReMixa

---
+++ Nodix/eLReMix.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nodix
+{
+    class eLReMix
+    {
+        List<int> klienty;
+
+        public eLReMix()
+        {
+            klienty = new List<int>();
+        }
+        public void setList(List <int> l)
+        {
+            klienty = new List<int>(l);
+        }
+        public void OgarnijPakiet(Packet.ATMPacket pkt)
+        {
+            //obsługa pakietu
+        }
+        public void Spamuj()
+        {
+            //wysyła do wszystkich sąsiadów z listy odpowiednie wiadomosci
+        }
+    }
+}
