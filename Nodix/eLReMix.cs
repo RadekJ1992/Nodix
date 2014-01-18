3a432505290b2fbae56d8bec681c3f73756744f0
vainamonen <vainamonen@gmail.com>
2014-01-18 15:35:03 +0100

    puszczam żeby Radek mógł dopisać myAddress
    i nie rozsynchronizował się

--- Nodix/eLReMix.cs
+++ Nodix/eLReMix.cs
@@ -9,10 +9,12 @@ namespace Nodix
     class eLReMix
     {
         List<int> klienty;
+        Nodix parent;
 
-        public eLReMix()
+        public eLReMix(Nodix nod)//ustawienie parenta
         {
             klienty = new List<int>();
+            parent = nod;
         }
         public void setList(List <int> l)
         {
@@ -21,10 +23,19 @@ namespace Nodix
         public void OgarnijPakiet(Packet.ATMPacket pkt)
         {
             //obsługa pakietu
+            int port = pkt.port;
+            byte[] payload = pkt.payload;
         }
         public void Spamuj()
         {
             //wysyła do wszystkich sąsiadów z listy odpowiednie wiadomosci
+            byte[] payload={new byte()};
+            parent.queuedReceivedPackets.Enqueue(new Packet.ATMPacket(Packet.ATMPacket.AALType.SSM, payload, 0,0);
         }
+        public void wyslijSPacket(Packet.SPacket cos)
+        {
+            
+        }
+        
     }
 }
