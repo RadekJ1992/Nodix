daa8258ea0eeea5f3386616efc5625881a5041c1
vainamonen <vainamonen@gmail.com>
2014-01-18 16:02:32 +0100

    pętla zrobiona

--- Nodix/eLReMix.cs
+++ Nodix/eLReMix.cs
@@ -22,15 +22,25 @@ namespace Nodix
         }
         public void OgarnijPakiet(Packet.ATMPacket pkt)
         {
-            //obsługa pakietu
+            //obsługa pakietu od innego LRMa
             int port = pkt.port;
             byte[] payload = pkt.payload;
         }
         public void Spamuj()
         {
-            //wysyła do wszystkich sąsiadów z listy odpowiednie wiadomosci
-            byte[] payload={new byte()};
-            parent.queuedReceivedPackets.Enqueue(new Packet.ATMPacket(Packet.ATMPacket.AALType.SSM, payload, 0,0);
+            //wysyła do wszystkich sąsiadów z listy odpowiednie wiadomosci do LRMów
+            for (int i = 0; i < klienty.Count;i++ )
+            {
+
+                String str = "";
+                byte[] payload = new byte[str.Length * sizeof(char)];
+                System.Buffer.BlockCopy(str.ToCharArray(), 0, payload, 0, payload.Length);
+                Packet.ATMPacket packiet = new Packet.ATMPacket(Packet.ATMPacket.AALType.SSM, payload, 0, 0);
+                packiet.port = klienty.ElementAt(i);
+                packiet.VCI = -1;
+                packiet.VPI = -1;
+                parent.queuedReceivedPackets.Enqueue(packiet);
+            }
         }
         public void wyslijSPacket(Packet.SPacket cos)
         {
